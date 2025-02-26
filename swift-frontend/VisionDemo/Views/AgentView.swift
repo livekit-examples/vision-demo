9fcc4020987f64da823ccc288b186231e48e8091
mattherzog <Herzog.Matt@gmail.com>
2025-02-26 00:07:29 -0600

    Tweaking loading circle to match center circle of visualizer

--- swift-frontend/VisionDemo/Views/AgentView.swift
+++ swift-frontend/VisionDemo/Views/AgentView.swift
@@ -20,29 +20,53 @@ struct AgentAudioVisualizer: View {
     @EnvironmentObject var room: Room
     @Environment(\.colorScheme) private var colorScheme
     @ObservedObject var agent: RemoteParticipant
-    
-    private var barSpacing: CGFloat { room.localParticipant.isCameraEnabled() ? 4 : 12}
+    @State private var pulse = false
+    @State private var animateVisualizer = false
+    private let barSpacingFactor: CGFloat = 0.035
+    private let barCount: CGFloat = 5
 
     var body: some View {
         GeometryReader { geometry in
-            let availableWidth = geometry.size.width - (barSpacing * 2)
-            let barWidth = (availableWidth - (4 * barSpacing)) / 5
-            let computedSpacingFactor = barSpacing / availableWidth
+            let totalSpacing = geometry.size.width * barSpacingFactor * (barCount + 2)
+            let barWidth = (geometry.size.width - totalSpacing) / barCount
 
             ZStack {
                 if let track = agent.firstAudioTrack {
                     BarAudioVisualizer(
                         audioTrack: track,
                         barColor: colorScheme == .dark ? .white : .black,
-                        barCount: 5,
-                        barSpacingFactor: computedSpacingFactor,
+                        barCount: Int(barCount),
+                        barSpacingFactor: barSpacingFactor,
                         barMinOpacity: 1
                     )
-                    .frame(width: availableWidth, height: availableWidth)
+                    .frame(width: geometry.size.width, height: geometry.size.width)
+                    .opacity(animateVisualizer ? 1 : 0)
+                    .animation(.easeInOut(duration: 0.5), value: animateVisualizer)
+                    .onAppear {
+                        animateVisualizer = true
+                    }
+
+                    Circle()
+                        .fill(colorScheme == .dark ? .white : .black)
+                        .frame(width: barWidth, height: barWidth)
+                        .opacity(animateVisualizer ? 0 : (pulse ? 1 : 0.2))
+                        .animation(.easeInOut(duration: 0.5), value: animateVisualizer)
+                        .onAppear {
+                            withAnimation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
+                                pulse.toggle()
+                            }
+                        }
                 } else {
                     Circle()
-                        .fill(.blue)
+                        .fill(colorScheme == .dark ? .white : .black)
                         .frame(width: barWidth, height: barWidth)
+                        .opacity(pulse ? 1 : 0.2)
+                        .animation(.easeInOut(duration: 0.5), value: pulse)
+                        .onAppear {
+                            withAnimation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
+                                pulse.toggle()
+                            }
+                        }
                 }
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
